a324730f92ca3fb0570dcbb4e0d36cba201ca07a
arcilya <145717396+arcilya@users.noreply.github.com>
2025-03-11 10:35:58 +0300

    containers (debian, ubuntu, alpine)

--- app.py
+++ app.py
@@ -3,27 +3,26 @@ import requests
 
 st.title("Аренда виртуальных машин и контейнеров")
 
+# Выбор типа сервиса
 service_type = st.selectbox("Выберите тип сервиса", ["Виртуальная машина (QEMU)", "Контейнер (Docker)"])
 
-
+# Выбор ОС
 os_options = {
-    "Виртуальная машина (QEMU)": ["Ubuntu", "CentOS", "Fedora"],
-    "Контейнер (Docker)": ["Ubuntu", "Alpine", "Nginx"]
+    "Виртуальная машина (QEMU)": ["Ubuntu", "Debian", "CentOS"],
+    "Контейнер (Docker)": ["Ubuntu", "Alpine", "Debian"]
 }
-
 os_choice = st.selectbox("Выберите ОС", os_options[service_type])
 
 # Выбор ресурсов
-cpu = st.slider("Количество CPU", 1, 4, 2)
-ram = st.slider("ОЗУ (MB)", 512, 4096, 1024)
+cpu = st.slider("CPU", 1, 4, 2)
+ram = st.slider("RAM (MB)", 512, 4096, 1024)
 
-# Кнопка создания ВМ
-if st.button("Создать"):
-    response = requests.post("http://localhost:5000/create_vm", json={
+# Кнопка запуска
+if st.button("Запустить"):
+    response = requests.post("http://localhost:5000/create", json={
         "type": service_type,
         "os": os_choice,
         "cpu": cpu,
         "ram": ram
     })
-    data = response.json()
-    st.write(f"ВМ создана! Доступ по SSH: `{data['ssh']}`")
+    st.write(response.json())
